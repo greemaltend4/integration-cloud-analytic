70da511697f877e0aef32c1adad5c3afde87bd61
greemaltend4 <dengestabiy@hotmail.com>
2025-11-01 09:40:43 +0000

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  salesforce: {
+    username: 'votre_utilisateur',
+    password: 'votre_mot_de_passe',
+    securityToken: 'votre_token_de_securite'
+  },
+  googleAnalytics: {
+    viewId: 'votre_view_id',
+    credentials: {
+      client_email: 'votre_email_client',
+      private_key: 'votre_cle_privee'
+    }
+  },
+  slack: {
+    webhookUrl: 'https://hooks.slack.com/services/votre/webhook/url'
+  }
+};
